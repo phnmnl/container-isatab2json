733d575f77db10809fb851ac49e4d99fd5c40b82
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-06-09 14:50:00 +0100

    Modify for galaxy wrapper

--- run_test.sh
+++ run_test.sh
@@ -9,7 +9,9 @@ wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatas
 wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatasets/master/tab/BII-I-1/a_proteome.txt -P /tmp/
 wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatasets/master/tab/BII-I-1/a_transcriptome.txt -P /tmp/
 
-run_tab2json.py /tmp/
+zip -r /BII-I-1.zip /tmp/
+
+run_tab2json.py /BII-I-1.zip
 
 # check that files were created
 if ! [ -e "/out.json" ]; then
