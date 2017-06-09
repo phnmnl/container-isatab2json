a2a428bb343c025117936a6274553783fd5b82a4
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-06-09 12:28:32 +0100

    Modify for galaxy wrapper

--- run_tab2json.py
+++ run_tab2json.py
@@ -3,18 +3,18 @@
 import sys
 import os
 import json
-
-src_dir = sys.argv[1]
+import shutil
+src_zip = sys.argv[1]
 
 try:
     from isatools.convert import isatab2json
 except ImportError as e:
     raise RuntimeError("Could not import isatools package")
-
-if not os.path.exists(src_dir):
-    print("File path to ISA-Tab files {} does not exist".format(src_dir))
+shutil.unpack_archive(src_zip, '/tmp/')
+if not os.path.exists('/tmp/'):
+    print("File path to ISA-Tab files {} does not exist".format('/tmp/'))
     sys.exit(0)
 
-my_json = isatab2json.convert(src_dir)
+my_json = isatab2json.convert('/tmp/')
 with open("out.json", 'w') as out_fp:
     json.dump(my_json, out_fp)
