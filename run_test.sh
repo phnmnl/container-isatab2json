6429851c0f296b496024a3fb5a53df8873fdd428
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-06-12 14:44:15 +0100

    Try fix build

--- run_test.sh
+++ run_test.sh
@@ -9,7 +9,9 @@ wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatas
 wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatasets/master/tab/BII-I-1/a_proteome.txt -P /tmp/
 wget --no-check-certificate https://raw.githubusercontent.com/ISA-tools/ISAdatasets/master/tab/BII-I-1/a_transcriptome.txt -P /tmp/
 
-zip -r /BII-I-1.zip /tmp/
+cd /tmp/
+zip -r /BII-I-1.zip
+cd /
 rm -r /tmp/
 
 run_tab2json.py /BII-I-1.zip
