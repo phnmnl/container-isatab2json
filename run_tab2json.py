17b65ca816db8c093ce1a4adbaa4cd968a4faa26
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-06-12 14:18:42 +0100

    Try fix build

--- run_tab2json.py
+++ run_tab2json.py
@@ -11,13 +11,9 @@ try:
 except ImportError as e:
     raise RuntimeError("Could not import isatools package")
 print("src_zip = " + src_zip)
-shutil.unpack_archive(filename=src_zip, extract_dir='/tmp/')
+shutil.unpack_archive(filename=src_zip)
 
-if not os.path.exists('/tmp/'):
-    print("File path to ISA-Tab files {} does not exist".format('/tmp/'))
-    sys.exit(0)
-
-my_json = isatab2json.convert(work_dir='/tmp/', validate_first=False, use_new_parser=True)
+my_json = isatab2json.convert(work_dir='/', validate_first=False, use_new_parser=True)
 with open("/out.json", 'w') as out_fp:
     json.dump(my_json, out_fp)
     with open("/out.json") as j_fp:
